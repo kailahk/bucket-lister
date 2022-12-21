719f881a0e9959632cc8ece8f9c8658dc1a69a3a
kailahk <kailahkorsh@me.com>
2022-12-20 19:44:05 -0500

    custom favicon, logo on landing page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -8,6 +8,7 @@ export default function AuthPage({ setUser }) {
 
     return (
         <main className="auth-page">
+            <img src="https://i.imgur.com/0OWZbqy.png" alt="Bucket Lister Logo" className='Bucket-Lister-Logo'/>
             <div className='auth-page-contents'>
                 <h1 className='auth-page-title'>&nbsp;Bucket Lister&nbsp;</h1>
                 <p className="slogan">actually do stuff</p>
